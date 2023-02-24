bca6dc729ac3c5eb8d4c764a37412eb812e104f2
Alonso Herreros <alonso.herreros.c@gmail.com>
2023-02-24 18:05:59 +0100

    Update project1.m (final?)

--- matlab/project1.m
+++ matlab/project1.m
@@ -1,9 +1,23 @@
-TheSting = audioread("thesting.wav");
-StarWars = audioread("starwars.wav");
+TheSting = audioread("audio\thesting.wav");
+StarWars = audioread("audio\starwars.wav");
 
-minLength = min(length(TheSting),length(StarWars))
+minLength = min(length(TheSting),length(StarWars));
 
 TheSting = TheSting(1:minLength);
 StarWars = StarWars(1:minLength);
 
 n = 0:minLength-1;
+M = length(n)/2;
+
+s1 = (0.5 + 0.5*cos(2*pi*n/M));
+TheSting_faded = TheSting .* s1';
+
+s2 = (0.5 + 0.5*cos(2*pi*n/M + pi));
+StarWars_faded = StarWars .* s2';
+
+%plot(n, s1, n, s2);
+
+x = TheSting_faded + StarWars_faded;
+
+playaudio(x);
+

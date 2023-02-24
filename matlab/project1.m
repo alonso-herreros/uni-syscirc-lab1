50ec5ff52d268fda1a4c544e6557b54a8eacb515
Alonso Herreros <alonso.herreros.c@gmail.com>
2023-02-24 18:32:17 +0100

    Fix transposition problems

--- matlab/project1.m
+++ matlab/project1.m
@@ -7,15 +7,15 @@ minLength = min(length(TheSting),length(StarWars));
 TheSting = TheSting(1:minLength);
 StarWars = StarWars(1:minLength);
 
-n = 0:minLength-1;
+n = (0:minLength-1)'; % Using transposition to make it vertical
 M = length(n)/2; % By setting this to 2 there will be 2 cycles of back and forth fading (4 transitions)
 
 %% Apply fading
 s1 = (0.5 + 0.5*cos(2*pi*n/M));
-TheSting_faded = TheSting .* s1'; % The ' is to transpose the vector
+TheSting_faded = TheSting .* s1;
 
 s2 = (0.5 + 0.5*cos(2*pi*n/M + pi));
-StarWars_faded = StarWars .* s2';
+StarWars_faded = StarWars .* s2;
 
 plot(n, s1, n, s2); %For testing purposes
 
